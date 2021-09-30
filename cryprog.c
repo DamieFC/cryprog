e41b7113888c6efc5b3959a8487bab0996d393f5
GNUOverYooouu <dtortoise99@gmail.com>
2021-09-30 08:36:16 -0700

    Changed thing

--- cryprog.c
+++ cryprog.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
-char *insults[3] = {
-  "You're a stupid pile of sh*t, were you born out of your mom's butthole?", 
-  "You're so stupid, how did you even get to the store to buy a computer?"
-}
-
-char *errors[3] = {
-  "Problem with your brain",
-  "You are so ugly the computer caught on fire"
-}
-
 int main() {
+  char *insults[3] = {
+    "You're a stupid pile of sh*t, were you born out of your mom's butthole?", 
+    "You're so stupid, how did you even get to the store to buy a computer?"
+  }
+
+  char *errors[3] = {
+    "Problem with your brain",
+    "You are so ugly the computer caught on fire"
+  }
   printf("Error: %s\n", errors[1]);
   printf("Location: between chair and keyboard.\n");
   printf("%s\n", insults[1]);
